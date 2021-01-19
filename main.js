0ab8f64c0505c10dc5fafe6d57cda74dd0d02b78
mg-abramishvili <mg@abramishvili.net>
2021-01-19 16:19:32 +0500

    .

--- main.js
+++ main.js
@@ -15,49 +15,47 @@ function createWindow () {
         height: 768,
         frame: true,
         fullscreen: false,
+        type: module,
         webPreferences: {
             devTools: true,
             webSecurity: false,
             nodeIntegration: false,
             contextIsolation: false
         }
-});
-
-//mainWindow.webContents.openDevTools();
-
-mainWindow.loadURL('https://google.com');
-
-mainWindow.on('closed', function () {
-    mainWindow = null
-})
-
-const UrlFilter = {
-    urls: [
-        '*://*.mail.ru/*',
-        '*://*.exler.ru/*',
-        '*://*.oper.ru/*',
-    ]
-};
-
-mainWindow.webContents.session.webRequest.onBeforeRequest(UrlFilter, (details, callback) => {
-    //console.log('onBeforeRequest details', details);
-    console.log(details.url);
-    const { url } = details;
-    const localURL = url.replace(details.url, 'http://localhost' )
-
-    callback({
-        cancel: false,
-        redirectURL: ( encodeURI(localURL ) )
     });
-});
 
-mainWindow.webContents.session.webRequest.onErrorOccurred((details) => {
-    console.log('error occurred on request');
-    console.log(details);
-})
+    //mainWindow.webContents.openDevTools();
+
+    mainWindow.loadURL('https://exler.ru');
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
 }
 
 app.on('ready', createWindow)
+
+app.on('web-contents-created', (_event, contents) => {
+    contents.on('will-navigate', (e, urlLink) => {
+        //console.log(urlLink);
+
+        const whitelist = [ "https://www.exler.ru/blog", "google.com", "google.ru" ];
+
+        let redirect = false;
+        for(let it of whitelist){
+          if(urlLink.match(it)){
+              redirect = false;
+              break;
+          }else{
+              redirect = true;
+          }
+        }
+        
+        if(redirect)
+            //console.log("redirect");
+            mainWindow.loadURL('https://google.com');
+    });
+});
 
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
