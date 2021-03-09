5e05798ff24131d334a7319eef055fd389474154
mg-abramishvili <mg@abramishvili.net>
2021-03-10 01:26:10 +0500

    .

--- main.js
+++ main.js
@@ -26,10 +26,18 @@ axios.get('http://localhost/api/domains')
     });
 })
 
+axios.get('http://localhost/api/homepage')
+.then(response => {
+    fs.writeFile('homepage.json', JSON.stringify(response.data), function (err) {
+    });
+})
+
 let rawdatalinks = fs.readFileSync('whitelistlinks.json');
 let whitelistlinks = JSON.parse(rawdatalinks);
 let rawdatadomains = fs.readFileSync('whitelistdomains.json');
 let whitelistdomains = JSON.parse(rawdatadomains);
+let rawdatahomepage = fs.readFileSync('homepage.json');
+let homepage = JSON.parse(rawdatahomepage);
 
 let mainWindow
 
@@ -50,7 +58,9 @@ function createWindow () {
 
     //mainWindow.webContents.openDevTools();
 
-    mainWindow.loadURL('https://exler.ru');
+    for(let hp of homepage) {
+        mainWindow.loadURL(hp);
+    }
 
     mainWindow.on('closed', function () {
         mainWindow = null
@@ -61,6 +71,11 @@ app.on('ready', createWindow)
 
 app.on('web-contents-created', (_event, contents) => {
     contents.on('will-navigate', (e, urlLink) => {
+
+        contents.on('new-window', (e, url) => {
+            e.preventDefault()
+            mainWindow.loadURL(url);
+        })
 
         let redirect = false;
 
@@ -73,25 +88,60 @@ app.on('web-contents-created', (_event, contents) => {
             }
         }*/
 
-        for(let itd of whitelistdomains) {
-            if(urlLink.match(itd)) {
-                redirect = false;
-                break;
+        let downloads = [
+            '.zip',
+            '.rar',
+            '.7z',
+            '.mp4',
+            '.wmv',
+            '.mkv',
+            '.avi',
+            '.mp3',
+            '.wav',
+            '.ogg',
+            '.xls',
+            '.xlsx',
+            '.doc',
+            '.docx',
+            '.ppt',
+            '.pptx',
+            '.jpg',
+            '.jpeg',
+            '.png',
+            '.tiff',
+            '.bmp',
+            '.gif',
+            '.fb2',
+            '.epub',
+            '.rtf',
+        ]
+
+        for(let dl of downloads) {
+            if(urlLink.match(dl)) {
+                e.preventDefault()
             } else {
-                for(let itl of whitelistlinks) {
-                    if(urlLink == itl) {
+                for(let itd of whitelistdomains) {
+                    if(urlLink.match(itd)) {
                         redirect = false;
                         break;
                     } else {
-                        redirect = true;
+                        for(let itl of whitelistlinks) {
+                            if(urlLink == itl) {
+                                redirect = false;
+                                break;
+                            } else {
+                                redirect = true;
+                            }
+                        }
                     }
                 }
             }
         }
         
         if(redirect)
-            //console.log("redirect");
-            mainWindow.loadURL('https://google.com');
+            for(let hp of homepage) {
+                mainWindow.loadURL(hp);
+            }
     });
 });
 
